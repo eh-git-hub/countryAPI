77b198b0930aa4aba87a172175a620483b2edfd3
Edgar Hernandez <EdgarHomeFolder@Edgars-MacBook-Pro.local>
2021-04-04 21:03:21 -0500

    Uncaught (in promise) TypeError: Cannot set property 'innerHTML' of null

---
+++ app.js
@@ -0,0 +1,35 @@
+const countryList = document.getElementById("countryList");
+
+//Test
+// let msg = "Hello, World!"
+// console.log(msg);
+
+//TODO: connect API and display info. 
+//TODO: need to generate a random number that corresponds to the index of the API array and displays it 
+//TODO: need to add an onclick method that activates the function random when clicked
+
+    fetch("https://restcountries.eu/rest/v2/name/United States of America").then( function(response) {
+       // Access the JSON in the response
+       response.json().then( function(json) {
+           console.log(json);
+           const countryArr = {};
+           countryArr['name'] = json[0].name;
+           countryArr['capital'] = json[0].capital;
+           countryArr['region'] = json[0].region;
+           countryArr['languages'] = json[0].languages[0].name;
+           countryArr['flag'] = json[0].flag;
+
+           console.log(countryArr);
+        
+           //Add HTML that includes the JSON data
+            countryList.innerHTML = 
+           `<ul>
+                <img src = ${countryArr.flag}>
+                <h1>Name ${countryArr.name}</h1>
+                <h2>Capital ${countryArr.capital} </h2>
+                <p>Region ${countryArr.region}</p>
+                <p>Language ${countryArr.languages}</p>
+            <ul>
+           `
+       });
+    });
