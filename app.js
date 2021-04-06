1a1efadf2edeb70762257fabbd096e0d9ac9b578
Edgar Hernandez <EdgarHomeFolder@Edgars-MacBook-Pro.local>
2021-04-05 20:54:59 -0500

    added some styling and button functionality

--- app.js
+++ app.js
@@ -9,35 +9,45 @@ const countryList = document.getElementById("countryList");
 // for (let i = 0; i <= countryArr.length; i++){
     
 // }
+
 fetch("https://restcountries.eu/rest/v2/name/United States of America").then( function(response) {
     // fetch("https://restcountries.eu/rest/v2/all").then( function(response) {
         // Access the JSON in the response
         response.json().then( function(json) {
-            //console.log(json);
+            // console.log(json);
             const countryArr = [];
-            countryArr['name'] = json[0].name;
-            countryArr['capital'] = json[0].capital;
-            countryArr['region'] = json[0].region;
-            countryArr['languages'] = json[0].languages[0].name;
-            countryArr['flag'] = json[0].flag;
+                countryArr['name'] = json[0].name;
+                countryArr['capital'] = json[0].capital;
+                countryArr['region'] = json[0].region;
+                countryArr['languages'] = json[0].languages[0].name;
+                countryArr['flag'] = json[0].flag;
             
-            countryArr.join();
+            // countryArr.join();
             console.log(countryArr);
             
     //         //Add HTML that includes the JSON data
             countryList.innerHTML = 
             `<ul class="card">
                 <img class = "card-image" src=${countryArr.flag}>
-                <h1 class = "card-name"> Name: ${countryArr.name}</h1>
-                <h2 class = "card-capital"> Capital: ${countryArr.capital}</h2>
-                <h2 class = "card-region"> Region: ${countryArr.region}</h2>
-                <h2 class = "card-language"> Language: ${countryArr.languages}</h2>
+                <li class = "card-name"> Name: ${countryArr.name}</li>
+                <li class = "card-capital"> Capital: ${countryArr.capital}</li>
+                <li class = "card-region"> Region: ${countryArr.region}</li>
+                <li class = "card-language"> Language: ${countryArr.languages}</li>
             <ul>`
         });
     });
 
 
     //TODO: need to generate a random number that corresponds to the index of the API array and displays it 
-    // let fruitArray = ["Apples", "Bananas", "Pears"];
-    // let randomFruit = fruitArray[Math.floor(Math.random()*fruitArray.length)];
-    // console.log(randomFruit);
+    let countryArray = ["United States of America", "Afghanistan", "Åland Islands", "Albania", "Algeria", "American Samoa"];
+    
+    function randomCountryGen(){
+        let country = document.getElementById("randomCountry");
+        let randomCountry = countryArray[Math.floor(Math.random()*countryArray.length)];
+        
+        country.innerHTML = 
+            `<p class="randomCountry">${randomCountry}</p>`
+        
+        return randomCountry;
+    }
+    
