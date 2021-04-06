f75e71916f058ebeb9741c7f43246412f21b1abe
Edgar Hernandez <EdgarHomeFolder@Edgars-MacBook-Pro.local>
2021-04-05 21:22:20 -0500

    change pics/info with button

--- app.js
+++ app.js
@@ -10,8 +10,43 @@ const countryList = document.getElementById("countryList");
     
 // }
 
-fetch("https://restcountries.eu/rest/v2/name/United States of America").then( function(response) {
+// fetch("https://restcountries.eu/rest/v2/name/United States of America").then( function(response) {
     // fetch("https://restcountries.eu/rest/v2/all").then( function(response) {
+    // fetch(`https://restcountries.eu/rest/v2/name/${randomCountry}?fullText=true`).then( function(response) {
+        // Access the JSON in the response
+        // response.json().then( function(json) {
+            // console.log(json);
+            // const countryArr = [];
+            //     countryArr['name'] = json[0].name;
+            //     countryArr['capital'] = json[0].capital;
+            //     countryArr['region'] = json[0].region;
+            //     countryArr['languages'] = json[0].languages[0].name;
+            //     countryArr['flag'] = json[0].flag;
+            
+            // countryArr.join();
+            // console.log(countryArr);
+            
+    //         //Add HTML that includes the JSON data
+    //         countryList.innerHTML = 
+    //         `<ul class="card">
+    //             <img class = "card-image" src=${countryArr.flag}>
+    //             <li class = "card-name"> Name: ${countryArr.name}</li>
+    //             <li class = "card-capital"> Capital: ${countryArr.capital}</li>
+    //             <li class = "card-region"> Region: ${countryArr.region}</li>
+    //             <li class = "card-language"> Language: ${countryArr.languages}</li>
+    //         <ul>`
+    //     });
+    // });
+
+
+    //TODO: need to generate a random number that corresponds to the index of the API array and displays it 
+    let countryArray = ["United States of America", "Afghanistan", "Åland Islands", "Albania", "Algeria", "American Samoa", "Andorra", "Angola", "Anguilla", "Antarctica", "Antigua and Barbuda", "Argentina"];
+    
+    function randomCountryGen(){
+        let country = document.getElementById("randomCountry");
+        let randomCountry = countryArray[Math.floor(Math.random()*countryArray.length)];
+        
+        fetch(`https://restcountries.eu/rest/v2/name/${randomCountry}?fullText=true`).then( function(response) {
         // Access the JSON in the response
         response.json().then( function(json) {
             // console.log(json);
@@ -37,14 +72,6 @@ fetch("https://restcountries.eu/rest/v2/name/United States of America").then( fu
         });
     });
 
-
-    //TODO: need to generate a random number that corresponds to the index of the API array and displays it 
-    let countryArray = ["United States of America", "Afghanistan", "Åland Islands", "Albania", "Algeria", "American Samoa", "Andorra", "Angola"];
-    
-    function randomCountryGen(){
-        let country = document.getElementById("randomCountry");
-        let randomCountry = countryArray[Math.floor(Math.random()*countryArray.length)];
-        
         country.innerHTML = 
             `<p class="randomCountry">${randomCountry}</p>`
         
